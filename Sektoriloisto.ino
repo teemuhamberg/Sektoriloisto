524cf6f65dcff4ba7106cc0c699e8361dd0ecd40
teemuhamberg <58030176+teemuhamberg@users.noreply.github.com>
2020-11-19 15:12:54 +0200

    Add files via upload

--- Sektoriloisto.ino
+++ Sektoriloisto.ino
@@ -1,9 +1,17 @@
-#define LedS1V 10
-#define LedS2V 5
+#define LedS1Vi 11
+#define LedS1Ke 10
+#define LedS1Pu 9
+#define LedS2Vi 6
+#define LedS2Ke 5
+#define LedS2Pu 3
 
 void setup(){
-  pinMode(LedS1V, OUTPUT); 
-  pinMode(LedS2V, OUTPUT); 
+  pinMode(LedS1Vi, OUTPUT); 
+  pinMode(LedS1Ke, OUTPUT); 
+  pinMode(LedS1Pu, OUTPUT); 
+  pinMode(LedS2Vi, OUTPUT); 
+  pinMode(LedS2Ke, OUTPUT); 
+  pinMode(LedS2Pu, OUTPUT); 
   Serial.begin(9600);
   Serial.println("Startup");
 }
@@ -13,9 +21,11 @@ byte tehoS1,tehoS2=0; // Muuttujat tehon säätämiseen
 bool countUpS1, countUpS2 = true; // Muuttujat suunnan määräämiseen
 
 void breathS1(){
-  analogWrite(LedS1V, tehoS1);
+  analogWrite(LedS1Vi, tehoS1);
+  analogWrite(LedS1Ke, tehoS1);
+  analogWrite(LedS1Pu, tehoS1);
   if( countUpS1 == true ){
-    tehoS1 = tehoS1 +1;//Kasvata muuttujan arvoa yhdellä
+    tehoS1 = tehoS1 +1;//Kaseta muuttujan arvoa yhdellä
   }else{
     tehoS1 = tehoS1 -1;//Pienennä muuttujan arvoa yhdellä
   }
@@ -30,7 +40,9 @@ void breathS1(){
 }
 
 void breathS2(){
-  analogWrite(LedS2V, tehoS2);
+  analogWrite(LedS2Vi, tehoS2);
+  analogWrite(LedS2Ke, tehoS2);
+  analogWrite(LedS2Pu, tehoS2);
   if( countUpS2 == true ){
     tehoS2 = tehoS2 +2;//Kasvata muuttujan arvoa yhdellä
   }else{
